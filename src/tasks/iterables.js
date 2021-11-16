665d89cd889b10917fde1b89234f2de7658b6d26
Andrey <2593147@gmail.com>
2021-11-16 10:56:12 +0200

    Update 16.11.21

--- src/tasks/iterables.js
+++ src/tasks/iterables.js
@@ -3,8 +3,35 @@
  * step - шаг, по-умолчанию = 1, необязательный параметр
  * ф-ция должна создать объект, сделать его итерируемым(iterable) и вернуть
  */
-const createIterable = (from, to, step=1) => {
+const createIterable = (from, to, step = 1) => {
+	const obj = {
+		from,
+		to,
+		step,
 
+		[Symbol.iterator] () {
+			return {
+				current: this.from - 1,
+				last: this.to,
+				step: this.step,
+				next() {
+					this.current += this.step;
+
+					return {
+						done: this.current >= this.last,
+						value: this.current
+					};
+				}
+			};
+		}
+	}
+
+	return obj;
 };
+
+for (let num of createIterable(1, 5)) {
+	console.log(num);
+	console.log(typeof num);
+}
 
 

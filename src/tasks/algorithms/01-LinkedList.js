92c8ef10da485ee214759cae5f89e991d9ab5f08
Andrey <2593147@gmail.com>
2022-02-07 22:03:50 +0200

    feat: implement LinkedList methods

--- src/tasks/algorithms/01-LinkedList.js
+++ src/tasks/algorithms/01-LinkedList.js
@@ -3,6 +3,14 @@
  * optional - значит, что параметр может быть, а может и не быть передан
  * индексы начинаются с 0
  */
+
+class Node {
+  constructor (value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class LinkedList {
     /**
      * values(optional) - массив значений, которыми нужно заполнить созданный список
@@ -18,7 +26,18 @@ class LinkedList {
      * @param value
      */
     add(value) {
+      if (this.length === 0) {
+        this.head = new Node(value);
+      } else {
+        let current = this.head;
 
+        while (current.next) {
+          current = current.next;
+        }
+
+        current.next = new Node(value);
+      }
+      this.length++;
     }
 
     /**
@@ -27,9 +46,32 @@ class LinkedList {
      * @param i
      * @param value
      */
-    insert(i, value) {
-
+  insert(i, value) {
+    if (i < 0 || this.length < i) {
+      throw 'Wrong position. Game over!';
     }
+
+    let node = new Node(value);
+
+    if (i === 0) {
+      node.next = this.head;
+      this.head = node;
+    } else {
+      let current = this.head;
+      let prev = null;
+      let index = 0;
+
+      while (index < i) {
+        prev = current;
+        current = current.next;
+        index++;
+      }
+
+      prev.next = node;
+      node.next = current;
+    }
+    this.length++;
+  }
 
     /**
      * метод возвращает значение с заданным индексом
@@ -37,7 +79,19 @@ class LinkedList {
      * @param i
      */
     get(i) {
+      if (this.length === 0) {
+        throw 'Error! The list is empty!';
+      }
 
+      let current = this.head;
+      let index = 0;
+
+      while (index < i) {
+        current = current.next;
+        index++;
+      }
+
+      return current.value;
     }
 
     /**
@@ -46,21 +100,43 @@ class LinkedList {
      * @param i
      */
     removeAt(i) {
+      if (i < 0 || this.length < i) {
+        throw 'Wrong position. Game over!';
+      }
 
+      let current = this.head;
+
+      if (i === 0) {
+        this.head = current.next;
+      } else {
+        let prev = null;
+        let index = 0;
+
+        while (index < i) {
+          prev = current;
+          current = current.next;
+          index++;
+        }
+
+        prev.next = current.next;
+      }
+
+      this.length--;
+      return current.value;
     }
 
     /**
      * возвращает самое первое значение в списке
      */
     getFirst() {
-
+      return this.get(0);
     }
 
     /**
      * возвращает самое последнее значение в списке
      */
     getLast() {
-
+      return this.get(this.length - 1);
     }
 
     /**
@@ -68,7 +144,10 @@ class LinkedList {
      * если список пустой - бросить ошибку
      */
     removeFirst() {
-
+      if (this.length === 0) {
+        throw 'Error! The list is empty!';
+      }
+     return this.removeAt(0);
     }
 
     /**
@@ -76,13 +155,18 @@ class LinkedList {
      * если список пустой - бросить ошибку
      */
     removeLast() {
-
+      if (this.length === 0) {
+        throw 'Error! The list is empty!';
+      }
+      return this.removeAt(this.length - 1);
     }
 
     /**
      * возвращает размер списка
      */
     size() {
-
+      return this.length;
     }
 }
+
+
